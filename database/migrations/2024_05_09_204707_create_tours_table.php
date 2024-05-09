6a02841c9d201cea895319daaa8457383bc45359
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-09 22:31:15 +0100

    tour corr

--- database/migrations/2024_05_09_204707_create_tours_table.php
+++ database/migrations/2024_05_09_204707_create_tours_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('tours', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignId('travel_id')->constrained('travels');
+            $table->foreignUuid('travel_id')->constrained('travels');
             $table->string('name');
             $table->date('starting_date');
-            $table->date('ending_date');
             $table->date('ending_date');
             $table->integer('price');
             $table->timestamps();
