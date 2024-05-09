0b442a385215475c8b2df0a5e7b045b3635aff8a
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-09 22:07:41 +0100

    changed to Uuids

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
-    use HasFactory, Sluggable;
+    use HasFactory, Sluggable, HasUuids;
 
     protected $table = 'travels'; 
 
@@ -19,6 +21,11 @@ class Travel extends Model
         'description',
        'number_of_days',
     ];
+
+     public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
 
     public function sluggable(): array
     {
@@ -35,4 +42,6 @@ class Travel extends Model
             get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
         );
     }
+
+   
 }
