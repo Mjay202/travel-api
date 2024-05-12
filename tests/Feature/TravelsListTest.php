0ec4319c99634e5dfa3c1422063b6dbca87b8eaa
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-12 11:28:22 +0100

    travel index list test completed

--- tests/Feature/TravelsListTest.php
+++ tests/Feature/TravelsListTest.php
@@ -14,6 +14,7 @@ class TravelsListTest extends TestCase
      */
 
      use RefreshDatabase;
+     
     public function test_travel_list_paginated_data_is_returned_correctly(): void
     {
         Travel::factory(16)->create(['is_public' => true]);
@@ -23,6 +24,19 @@ class TravelsListTest extends TestCase
         $response->assertStatus(200);
         $response->assertJsonCount(15, 'data');  
         $response->assertJsonPath('meta.last_page', 2);  
+
+    }
+
+    public function test_travel_list_shows_only_public_records(): void
+    {
+        $publicTravel = Travel::factory()->create(['is_public' => true]);
+        Travel::factory()->create(['is_public' => false]);
+
+        $response = $this->get('/api/v1/travel');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');  
+        $response->assertJsonPath('data.0.id', $publicTravel->id);  
 
     }
 }
