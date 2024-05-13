eab4d49ee7489188d4d7f876699ccdee489a27cb
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-13 17:53:21 +0100

    tour_list_test_for_sorting_by_price

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -15,6 +15,7 @@ class TourResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'travel_id' => $this->travel_id,
             'name' => $this->name,
             'starting_date' => $this->starting_date,
--- tests/Feature/TourListTest.php
+++ tests/Feature/TourListTest.php
@@ -57,5 +57,100 @@ class TourListTest extends TestCase
         $response->assertJsonPath('meta.last_page', 2);
     }
 
+    public function test_tours_list_is_sorted_by_starting_date(): void
+    {
+       $travel = Travel::factory()->create();
+        
+       $laterTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'starting_date' => now()->addDays(5),
+        ]);
+        
+       
+       $earlierTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'starting_date' => now(),
+        ]);
 
+        $response = $this->get('/api/v1/travel/'.$travel->slug.'/tours');
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.id', $earlierTour->id);
+        $response->assertJsonPath('data.1.id', $laterTour->id);
+        $response->assertJsonCount(2, 'data');
+    }
+
+     public function test_tours_list_sorts_by_price_correctly(): void
+    {
+        $travel = Travel::factory()->create();
+        $cheapLaterTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 111,
+            'starting_date' => now()->addDays(5),
+        ]);
+        
+       $expensiveEarlierTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 888,
+            'starting_date' => now(),
+        ]);
+
+       $cheapEarlierTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 111,
+            'starting_date' => now(),
+        ]);
+
+       $expensiveLaterTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 888,
+            'starting_date' => now()->addDays(5),
+        ]);
+        
+        $response = $this->get('/api/v1/travel/'.$travel->slug.'/tours?sortBy=price&sortOrder=asc');
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.id', $cheapEarlierTour->id);
+        $response->assertJsonPath('data.1.id', $cheapLaterTour->id);
+        $response->assertJsonPath('data.2.id', $expensiveEarlierTour->id);
+        $response->assertJsonPath('data.3.id', $expensiveLaterTour->id);
+       
+    }
+
+     public function test_tours_list_filters_price_correctly(): void
+    {
+        $travel = Travel::factory()->create();
+        $cheapLaterTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 111,
+            'starting_date' => now()->addDays(5),
+        ]);
+        
+       $expensiveEarlierTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 888,
+            'starting_date' => now(),
+        ]);
+
+       $cheapEarlierTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 111,
+            'starting_date' => now(),
+        ]);
+
+       $expensiveLaterTour = Tour::factory()->create([
+            'travel_id' => $travel->id,
+            'price' => 888,
+            'starting_date' => now()->addDays(5),
+        ]);
+        
+        $response = $this->get('/api/v1/travel/'.$travel->slug.'/tours?sortBy=price&sortOrder=asc');
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.id', $cheapEarlierTour->id);
+        $response->assertJsonPath('data.1.id', $cheapLaterTour->id);
+        $response->assertJsonPath('data.2.id', $expensiveEarlierTour->id);
+        $response->assertJsonPath('data.3.id', $expensiveLaterTour->id);
+       
+    }
 }
