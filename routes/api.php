b9facd0591b8ec9373ba8e245f7c546a450465a9
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-18 10:38:51 +0100

    LooginRequest

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Admin\TravelController as AdminTravelController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourContoller;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -17,3 +18,5 @@ Route::get('travels/{travel:slug}/tours', [TourContoller::class, 'index']);
 Route::prefix('admin')->group(function () {
     Route::post('travels', [AdminTravelController::class, 'store']);
 });
+
+Route::post('/login', [LoginController::class, 'login']);
