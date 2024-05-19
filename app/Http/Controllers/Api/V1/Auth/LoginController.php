66b78492c47daa35de8b5fa27804a0fd84c0bb07
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-19 20:43:18 +0100

    accesstoken works

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
 
        
         $device = substr($request->userAgent()?? 0, 255);
-        $curUser = $request->user();
        return response()->json([
             'access_token' => $user->createToken($device)->plainTextToken,
         ]);
