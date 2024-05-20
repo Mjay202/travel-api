fcc53f2abdf5d75131174993b9363895e9982aa6
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-20 12:28:11 +0100

    login_test returns token for validated users

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    use RefreshDatabase;
+
+    public function test_login_returns_token_for_validated_users(): void
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->postJson('/api/v1/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['access_token']);
+    }
+}
