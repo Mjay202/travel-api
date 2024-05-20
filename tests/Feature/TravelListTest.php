de0e8d92307091f82e73bc3c0ea95de986473c7c
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-05-20 12:50:46 +0100

    adminTravelListTest non_admin cannnot create travel

--- tests/Feature/TravelListTest.php
+++ tests/Feature/TravelListTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Role;
+use App\Models\User;
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,11 +15,22 @@ class TravelListTest extends TestCase
      * A basic feature test example.
      */
     use RefreshDatabase;
-    public function test_public_user_cannot_create_travel(): void
+    public function test_public_user_cannot_create_travels(): void
     {
         
         $response = $this->postJson('/api/v1/admin/travels');
 
         $response->assertStatus(401);
+    }
+    
+    public function test_non_admin_users_cannot_create_travels(): void
+    {
+        $user = User::factory()->create();
+        $this->seed(RoleSeeder::class);
+        $user->roles()->attach(Role::where('name', 'editor')->value('id'));
+
+        $response = $this->actingAs($user)->postJson('/api/v1/admin/travels');
+
+        $response->assertStatus(403);
     }
 }
